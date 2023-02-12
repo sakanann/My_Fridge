3bf9bde45990a466d99d71dca3930a07007b01bb
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-13 01:00:22 +0900

    fix:devise　## Trackable　カラム修正

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,12 @@ ActiveRecord::Schema.define(version: 2023_02_12_152035) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
