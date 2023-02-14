f0ef665c957072ff18b96dec6389f4c9883992e0
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-14 14:06:32 +0900

    国際化試したが適用不可-保留

--- app/controllers/food_stocks_controller.rb
+++ app/controllers/food_stocks_controller.rb
@@ -3,7 +3,8 @@ class FoodStocksController < ApplicationController
   before_action :authenticate_user!
   # GET /food_stocks or /food_stocks.json
   def index
-    @food_stocks = FoodStock.all
+    # @food_stocks = FoodStock.all
+    @food_stocks = current_user.food_stocks
   end
 
   # GET /food_stocks/1 or /food_stocks/1.json
@@ -24,38 +25,31 @@ class FoodStocksController < ApplicationController
     @food_stock = FoodStock.new(food_stock_params)
     @food_stock = current_user.food_stocks.build(food_stock_params)
 
-    respond_to do |format|
-      if @food_stock.save
-        format.html { redirect_to food_stock_url(@food_stock), notice: "Food stock was successfully created." }
-        format.json { render :show, status: :created, location: @food_stock }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food_stock.errors, status: :unprocessable_entity }
-      end
+    if @food_stock.save
+    # binding.pry
+      flash[:notice] = "食材を登録しました！"
+      redirect_to food_stocks_path
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /food_stocks/1 or /food_stocks/1.json
   def update
-    respond_to do |format|
-      if @food_stock.update(food_stock_params)
-        format.html { redirect_to food_stock_url(@food_stock), notice: "Food stock was successfully updated." }
-        format.json { render :show, status: :ok, location: @food_stock }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food_stock.errors, status: :unprocessable_entity }
-      end
+    # @food_stock = food_stock.find(params[:id])
+    if @food_stock.update(food_stock_params)
+      flash[:notice] = "食材情報を修正しました！"
+      redirect_to food_stocks_path
+    else
+      render :edit
     end
   end
 
   # DELETE /food_stocks/1 or /food_stocks/1.json
   def destroy
     @food_stock.destroy
-
-    respond_to do |format|
-      format.html { redirect_to food_stocks_url, notice: "Food stock was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "食材を削除しました"
+    redirect_to food_stocks_path
   end
 
   private
