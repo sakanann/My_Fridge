f2f20f8264b0471af23a931e4e5c6163ee8d1df9
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-14 15:20:49 +0900

    add:ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :food_stocks
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   resources :food_stocks
   root 'food_stocks#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  # if Rails.env.development?
+  #   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  # end
+  }
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    #ゲストログイン用のパスを追加
   end
 end
