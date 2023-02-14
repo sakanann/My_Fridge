644cdddba8922558dbd49d12aa57eba25c38fafc
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-14 15:55:48 +0900

    add:管理者ゲストログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,17 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
+
+  def self.admin_guest
+    find_or_create_by!(email: 'admin_guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "管理者ゲストユーザー"
+      user.admin = true
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
 
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
     #ゲストログイン用のパスを追加
   end
 end
