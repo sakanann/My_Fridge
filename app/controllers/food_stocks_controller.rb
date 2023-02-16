1c055cb03e7dd3493be1bca6c0d9457351e90ebb
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-17 00:43:58 +0900

    add:カレンダー　土日カラー　日曜はじまり

--- app/controllers/food_stocks_controller.rb
+++ app/controllers/food_stocks_controller.rb
@@ -1,7 +1,7 @@
 class FoodStocksController < ApplicationController
   before_action :set_food_stock, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
-
+  before_action :set_beginning_of_week
 
   def index
     # @food_stocks = FoodStock.all
@@ -52,12 +52,18 @@ class FoodStocksController < ApplicationController
   end
 
   private
-    def set_food_stock
-      @food_stock = FoodStock.find(params[:id])
-    end
+#日曜日を始まり(一番左) gem simple_calendar
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
+  end
 
 
-    def food_stock_params
-      params.require(:food_stock).permit(:use_up_on, :price, :consumption, :notes, :food_id)
-    end
+  def set_food_stock
+    @food_stock = FoodStock.find(params[:id])
+  end
+
+
+  def food_stock_params
+    params.require(:food_stock).permit(:use_up_on, :price, :consumption, :notes, :food_id)
+  end
 end
