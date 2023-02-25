4573d535c386b1b311d0bdf809524360c4cfd7c9
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-25 17:56:17 +0900

    add:タイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module MyFridge
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
