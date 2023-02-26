258c7db56ad15eb8fe8176fc51581f047be11a45
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-26 20:06:37 +0900

    add:foodモデルバリデーション追記

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,8 @@ class Food < ApplicationRecord
   has_many :user, through: :food_stocks
   belongs_to :food_category
 
+  #食品名バリデーション：空欄ダメだよ！
+  validates :name, presence: true
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "food_category_id", "id", "name", "updated_at"]
   end
