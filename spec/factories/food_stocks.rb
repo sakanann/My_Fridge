341f2bbba9c91041d5c9abc121145dab22fa5eec
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-26 21:59:48 +0900

    add:model spec足掻き実験中

--- spec/factories/food_stocks.rb
+++ spec/factories/food_stocks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :food_stock do
-    
+    use_up_on { '坂本' }
+    price { 100 }
+    consumption { false }
+    notes { 'memo' }
   end
 end
--- spec/models/food_stock_spec.rb
+++ spec/models/food_stock_spec.rb
@@ -2,48 +2,48 @@ require 'rails_helper'
 
 RSpec.describe FoodStock, type: :model do
   describe 'バリデーションのテスト' do
-    let(:food) { create(:food) }
-    let(:user) { create(:user) }
-    
-    it '必要な情報が全て入力されていれば有効であること' do
-      food_stock = FoodStock.new(use_up_on: 2023, 9 , 6 , price: 500, consumption: false, notes: "ai")
-      expect(food_stock).to be_valid
+    context '食材リストの価格カラムが空の場合' do
+      it 'バリデーションにひっかかる' do
+        food_stock = FoodStock.new(price: '')
+        expect(food_stock).not_to be_valid
+      end
     end
-    
-    it 'userが存在しなければ無効であること' do
-      food_stock = build(:food_stock, user: nil)
-      food_stock.valid?
-      expect(food_stock.errors[:user]).to include('を入力してください')
-    end
-    
-    it 'foodが存在しなければ無効であること' do
-      food_stock = build(:food_stock, food: nil)
-      food_stock.valid?
-      expect(food_stock.errors[:food]).to include('を入力してください')
-    end
-    
-    it 'use_up_onが過去の日付であれば無効であること' do
-      food_stock = build(:food_stock, use_up_on: Date.yesterday, food: food, user: user)
-      food_stock.valid?
-      expect(food_stock.errors[:use_up_on]).to include('は今日以降のものを選択してください')
-    end
-  end
-  
-  describe 'メソッドのテスト' do
-    let(:food) { create(:food) }
-    let(:user) { create(:user) }
-    
-    it 'start_timeメソッドが期待通り機能すること' do
-      food_stock = build(:food_stock, use_up_on: '2023-03-01', food: food, user: user)
-      expect(food_stock.start_time).to eq(food_stock.use_up_on)
-    end
-    
-    it 'ransackable_attributesメソッドが期待通り機能すること' do
-      expect(FoodStock.ransackable_attributes).to include("consumption", "created_at", "food_id", "id", "notes", "price", "updated_at", "use_up_on", "user_id")
-    end
-    
-    it 'ransackable_associationsメソッドが期待通り機能すること' do
-      expect(FoodStock.ransackable_associations).to include("food", "user")
+
+    context '食材リストの価格カラムがきちんと設定されている場合' do
+      it 'バリデーションに通る' do
+        food = FactoryBot.create(:food)
+        food_stock = FoodStock.new(price: 100, notes: 'memo', food_id: food.id)
+        expect(food_stock).to be_valid
+      end
     end
   end
 end
+
+#   describe '検索機能' do
+#     # 必要に応じて、テストデータの内容を変更して構わない
+#     let!(:foods_stock) { FactoryBot.create(:foods_stock, title: '坂本') }
+#     let!(:second_foods_stock) { FactoryBot.create(:second_foods_stock, title: "sample") }
+#     context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+#       it "検索キーワードを含むタスクが絞り込まれる" do
+#         # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+#         expect(foods_stock.scope_title('坂本')).to include(foods_stock)
+#         expect(foods_stock.scope_title('坂本')).not_to include(second_foods_stock)
+#         expect(foods_stock.scope_title('坂本').count).to eq 1
+#       end
+#     end
+#     context 'scopeメソッドでステータス検索をした場合' do
+#       it "ステータスに完全一致するタスクが絞り込まれる" do
+#         expect(foods_stock.where(status: '着手中')).to include(foods_stock)
+#         expect(foods_stock.where(status: '着手中')).not_to include(second_foods_stock)
+#         expect(foods_stock.where(status: '着手中').count).to eq 1
+#       end
+#     end
+#     context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+#       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+#         expect(foods_stock.scope_title('坂').where(status: "着手中")).to include(foods_stock)
+#         expect(foods_stock.scope_title('坂').where(status: "着手中")).not_to include(second_foods_stock)
+#         expect(foods_stock.scope_title('坂').where(status: "着手中").count).to eq 1
+#       end
+#     end
+#   end
+# end
