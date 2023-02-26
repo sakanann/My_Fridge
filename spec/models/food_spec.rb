92cc614b2869066e8da5e92944c50bcdd7222a4d
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-27 00:26:58 +0900

    add:food_spec.rb実装Ok

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:food_category) { FoodCategory.create(name: 'テストカテゴリー') }
+  let(:food) { Food.new(name: 'テスト食品', food_category: food_category) }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(food).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      food.name = nil
+      expect(food).not_to be_valid
+    end
+  end
+
+  # describe 'associations' do
+  #   it { should belong_to(:food_category) }
+  #   it { should have_many(:food_stocks).dependent(:destroy) }
+  #   it { should have_many(:users).through(:food_stocks) }
+  # end
 end
