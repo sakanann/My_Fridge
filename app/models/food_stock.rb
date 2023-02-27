6ce704cce773b31d4c9eb8135e9e5d22061e088b
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-27 20:17:34 +0900

    fix:バリデーション　price >=0

--- app/models/food_stock.rb
+++ app/models/food_stock.rb
@@ -2,10 +2,11 @@ class FoodStock < ApplicationRecord
   belongs_to :user
   belongs_to :food
 
-#価格のバリデーション：空欄はアカンよ（0円でもいいから入力してのメッセージエラー文つけな！）
-  validates :price, presence: true
+#価格のバリデーション：空欄はアカンよ（0円でもいいから入力してのメッセージエラー文つけな！）－許さんぞってバリデーション
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 #メモのバリデーション：最大記述30文字ですよ！
   validates :notes, length: { maximum: 30 }
+
 
 
   def start_time
