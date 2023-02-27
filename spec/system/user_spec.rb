76d26dc54b80ab5aee87cbc4eb6466bde27b8c72
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-02-27 17:20:56 +0900

    add:userモデルテスト修正　システムスペック記述

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザー管理機能' , type: :system do
-  let(:user) { FactoryBot.create(:user) }
-  let(:user1) { FactoryBot.create(:user1) }
-  let(:user2) { FactoryBot.create(:user2) }
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:user1) { FactoryBot.create(:user1) }
+  let!(:user2) { FactoryBot.create(:user2) }
 
 
-# def login
-#   visit new_session_path
-#   fill_in 'session_email', with: 'user1@dive.com'
-#   fill_in 'session_password', with: '222222'
-#   click_on "Log in"
-# end
+  def log_in
+    visit new_user_session_path
+    fill_in 'user[email]', with: 'user1@dive.com'
+    fill_in 'user[password]', with: '222222'
+    click_button "ログイン"
+  end
 
 
   describe '一般ユーザ登録のテスト' do
@@ -32,11 +32,8 @@ RSpec.describe 'ユーザー管理機能' , type: :system do
     context '正しいパスワードが入力された場合' do
       it 'ログインできること' do
         visit new_user_session_path
-        fill_in 'user[email]', with: 'tokyo@example.com'
-        fill_in 'user[password]', with: 'password'
-        click_on 'アカウント登録'
-        #binding.pry
-        expect(page).not_to have_content 'ログインしています'
+        log_in
+        expect(page).to have_content 'ログインしています'
       end
     end
 
@@ -44,10 +41,9 @@ RSpec.describe 'ユーザー管理機能' , type: :system do
     context '間違ったパスワードが入力された場合' do
       it 'ログインできないこと' do
         visit new_user_session_path
-        fill_in 'user[email]', with: 'tokyo@example.com'
-        fill_in 'user[password]', with: 'pass'
-        click_on 'アカウント登録'
-        #binding.pry
+        fill_in 'user[email]', with: 'user1@dive.com'
+        fill_in 'user[password]', with: '22'
+        click_button "ログイン"
         expect(page).not_to have_content 'ログインしています'
       end
     end
@@ -59,24 +55,12 @@ RSpec.describe 'ユーザー管理機能' , type: :system do
         visit new_user_session_path
         fill_in 'user[email]', with: 'admin@dive.com'
         fill_in 'user[password]', with: '111111'
-        click_on 'アカウント登録'
-        click_on '管理者'
-        expect(page).to have_content '管理者'
+        click_button 'ログイン'
+        click_link '管理者ページへ'
+        sleep(5)
+        expect(page).to have_content 'サイト管理'
       end
     end
   end
 end
 
-
-# describe '一般ユーザログインのテスト' do
-#   context 'ログアウトテスト' do
-#     it 'ユーザーがログアウトする' do
-#       visit new_user_session_path
-#       fill_in 'user[email]', with: 'example@example.com'
-#       fill_in 'user[password]', with: 'password'
-#       click_on 'commit'
-#       click_on 'ログアウト'
-#       expect(page).to have_content 'ログアウトしました。'
-#     end
-#   end
-# end
